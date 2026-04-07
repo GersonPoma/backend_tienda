2fdfd6a2939d1f37864eeca7b2a49fd6bb4eb58e
Gerson <villarroelpoma2@gmail.com>
2026-04-07 16:22:54 -0400

    paginacion

--- backend_tienda/settings.py
+++ backend_tienda/settings.py
@@ -73,6 +73,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # 10 resultados por página
 }
 
 # Configuración de JWT
